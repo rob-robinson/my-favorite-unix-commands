e68a67035ae32033ee7f61e0920ae236e0608625
Rob Robinson <rorobinson@csufresno.edu>
2013-10-16 16:37:29 -0700

    adding system monitoring

--- find and grep.sh
+++ find and grep.sh
@@ -1,6 +1,9 @@
 # find and list all files with .jpg in their name within specified directory over 500k in size
 find /var/www/htdocs/ -name ".jpg" -a  -size +500k -ls
 
+# find and list all files larger than 10,000k
+find /var/www/htdocs/ -size +10000k -exec ls -lah {} \;
+
 # find all apache log entries that are for php and html and occured on specified date, print requestor ipaddress, requested file, and ...
 # count unique items, and display results, starting with the most occurances first
 grep -E '(php|html) HTTP'  /var/www/log/access_log | grep '30/Aug/2013:12' | awk '{print $1,$6,$7}' | sort | uniq -c | sort -nr
---
+++ system-monitoring.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# :: apache
+service httpd fullstatus
+# get all currently established http connections 
+netstat -pant | grep httpd | grep -c ESTAB
+
+# get all currently waiting http connections
+netstat -pant | grep httpd | grep -c WAIT
+
+# :: network
+netstat -s
+
+# :: mysql
+mysqladmin status
+mysqladmin -v processlist
+
+# :: system
+
+# get the ID or Product ID from your storage controller:
+omreport storage pdisk controller=0 | egrep "(^ID|^Product\ ID)"
+
+# all processes and the parameters that were called with them
+ps auxww
+
+# top ten CPU hogs
+ps auxww --sort=-pcpu | head -10
+
+# a tree version of ps
+pstree
+
+# how long has the server been up since last reboot
+uptime
+
+# show information about memory and swap space
+free
+
+# Report virtual memory statistics
+vmstat
+
+# Report of cpu, device, and nsf utilization
+iostat
+
+# interval reports on various system statistics...
+sar
